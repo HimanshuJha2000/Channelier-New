fa5313ffb279caf5c5531fca58c3d031edc19a6d
Himanshu Jha <anon99calpha@gmail.com>
2019-11-14 02:32:49 +0530

    Storyboards added

--- Channelier new/Controller/Profile VC/ProfileVC.swift
+++ Channelier new/Controller/Profile VC/ProfileVC.swift
@@ -10,23 +10,29 @@ import UIKit
 
 class ProfileVC: UIViewController {
 
-    @IBOutlet weak var backBtn: UIButton!
+
+    @IBAction func backBtn(_ sender: UIButton) {
+        let transition: CATransition = CATransition()
+        transition.duration = 0.4
+        transition.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+        transition.type = CATransitionType.reveal
+        transition.subtype = CATransitionSubtype.fromRight
+        self.view.window!.layer.add(transition, forKey: nil)
+        guard let HomeViewController = storyboard?.instantiateViewController(identifier: "HomeViewController") as? HomeViewController else { return }
+        HomeViewController.modalPresentationStyle = .overCurrentContext
+        present(HomeViewController, animated: false)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let label:UILabel = UILabel(frame: CGRect(x: 140, y: 30, width: 150, height: 30))
+        label.text = "Profile"
+        label.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        label.font = UIFont.systemFont(ofSize: 22.0)
+        self.view.addSubview(label)
 
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
